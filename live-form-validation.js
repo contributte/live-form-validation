ffd51116c0409341642098721f47e3541a127ffc
daremachine <d4red3vill@gmail.com>
2016-09-02 02:08:55 +0200

    opraví funkce toggle na id

--- live-form-validation.js
+++ live-form-validation.js
@@ -1004,8 +1004,7 @@ Nette.toggleControl = function(elem, rules, success, firsttime, value) {
 
 			} else if (rule.neg) {
 				curSuccess = !curSuccess;
-			}
-			if (!rule.rules) {
+			} else if (!rule.rules) {
 				success = curSuccess;
 			}
 		}
