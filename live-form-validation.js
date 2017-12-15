44aaa940aee09bcdad0783c95f43b998c8c48ffe
Matěj Polák <matejpolak42@gmail.com>
2017-12-15 15:20:11 +0100

    Fixed range validator for min & max validator #38
    Should resolve issue #38

--- live-form-validation.js
+++ live-form-validation.js
@@ -957,7 +957,8 @@ Nette.validators = {
 
 	range: function(elem, arg, val) {
 		if (elem.type === 'number') {
-			if (elem.validity.rangeUnderflow || elem.validity.rangeOverflow) {
+			if ((elem.validity.rangeUnderflow && arg[0] !== null) || (elem.validity.rangeOverflow && arg[1] !== null)) {
+				debugger;
 				return false
 			} else if (elem.validity.badInput) {
 				return null;
